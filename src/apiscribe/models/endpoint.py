a5a065f1ba95b053e0fcf2f24ea349071c402795
ilyshks <ilyagorunov.04@gmail.com>
2026-03-08 19:15:13 +0300

    expand endpoint model

--- src/apiscribe/models/endpoint.py
+++ src/apiscribe/models/endpoint.py
@@ -1,9 +1,15 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Dict
 
 
 class EndpointModel(BaseModel):
+
     path: str
     method: str
+
     request_schema: Optional[dict] = None
     response_schema: Optional[dict] = None
+
+    request_count: int = 0
+
+    request_field_counts: Dict[str, int] = {}
